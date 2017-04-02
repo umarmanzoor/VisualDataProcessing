a6ae06084c23a7b934cfeb6910cbc6e9a82e8681
Umar Manzoor <umarmanzoor@gmail.com>
2017-04-02 15:50:06 +0300

    Bug fixed in Relations

--- objectsLRRelationFinder.m
+++ objectsLRRelationFinder.m
@@ -14,9 +14,6 @@ for k = 1 : size(MyFolderInfo)
 	fid = fopen(file);
     outputfid = fopen(outputfile,'w');
 	tline = fgetl(fid);
-    originalImage = imread(image);
-
-    figure, imshow(originalImage);
 
     while ischar(tline)
         objects = strsplit(tline, ',');
@@ -25,6 +22,10 @@ for k = 1 : size(MyFolderInfo)
         label = char(filename(1));
         image = strcat('C:\Users\Umar Manzoor\Documents\GitHub\saul\data\mSprl\matlabcode\00\images\', label);
         image = strcat(image, '.jpg');
+    
+        originalImage = imread(image);
+
+%       figure, imshow(originalImage);
                        
 	    Mask_1 = strcat(label, '_');
 	    Mask_1 = strcat(Mask_1, char(objects(1)));
@@ -43,7 +44,7 @@ for k = 1 : size(MyFolderInfo)
 
         boundary1 = boundaryLocator(originalImage, firstObjMask);
 
-        figure, plot(boundary1(:,2), boundary1(:,1), 'r', 'LineWidth', 3);
+%        figure, plot(boundary1(:,2), boundary1(:,1), 'r', 'LineWidth', 3);
 
         axis([0 columns 0 rows]);
 
@@ -53,50 +54,42 @@ for k = 1 : size(MyFolderInfo)
 
         boundary2 = boundaryLocator(originalImage, secondObjMask);
 
-        plot(boundary2(:,2), boundary2(:,1), 'r', 'LineWidth', 3);
+%        plot(boundary2(:,2), boundary2(:,1), 'r', 'LineWidth', 3);
 
         [x1,y1,x2,y2,minDistance, maxx1, maxx2, maxy1, maxy2] = calculateMinDistance(boundary1, boundary2);
         percent = disjointObjects(boundary1, boundary2);
         
-        line([x1, x2], [y1, y2], 'Color', 'y', 'LineWidth', 3);              
+%        line([x1, x2], [y1, y2], 'Color', 'y', 'LineWidth', 3);              
         
         if((minDistance~=0) && strcmp(xyaxis, 'y-aligned'))
             if (x2 - x1 > 0)
-                % o1 is on left of o2
-                fprintf(outputfid,'Left(%s, %s)\n',char(objects{4}), char(objects{5}));
+                fprintf(outputfid,'Left,%s,%s,%s,%s \r\n',char(objects{1}),char(objects{4}),char(objects{2}), char(objects{5}));
             elseif (x2 - x1 < 0)
-                % o2 is on left of o1
-                fprintf(outputfid,'Right(%s, %s)\n',char(objects{5}), char(objects{4}));
+                fprintf(outputfid,'Right,%s,%s,%s,%s \r\n',char(objects{1}),char(objects{4}),char(objects{2}), char(objects{5}));                
             end
         elseif ((minDistance~=0) && strcmp(xyaxis, 'x-aligned'))
-            if (y2 - y1 > 0)
-                % o2 is above of o1
-                fprintf(outputfid,'Above(%s, %s)\n',char(objects{5}), char(objects{4}));
-            elseif (y2 - y1 < 0)
-                % o2 is below of o1
-                fprintf(outputfid,'Below(%s, %s)\n',char(objects{4}), char(objects{5}));
+            if (y2 - y1 > 0)2
+                fprintf(outputfid,'Above,%s,%s, %s,%s \r\n',char(objects{1}),char(objects{4}),char(objects{2}), char(objects{5}));
+            elseif (y2 - y1 < 0)                
+                fprintf(outputfid,'Below,%s,%s,%s,%s \r\n',char(objects{2}),char(objects{5}),char(objects{1}), char(objects{4}));                
             end
         elseif(minDistance==0 && percent > 80.00)
                 output = 'Ignore';
         elseif((minDistance==0) && strcmp(xyaxis, 'y-aligned'))
             if (maxx1 - maxx2 > 0)
-                % o1 is on left of o2
-                fprintf(outputfid,'Left(%s, %s)\n',objects{4}, objects{5});
+                fprintf(outputfid,'Left,%s,%s,%s,%s \r\n',char(objects{1}),char(objects{4}),char(objects{2}), char(objects{5}));
             elseif (maxx2 - maxx1 < 0)
-                % o2 is on left of o1
-                fprintf(outputfid,'Right(%s, %s)\n',objects{5}, objects{4});
+                fprintf(outputfid,'Right,%s,%s,%s,%s \r\n',char(objects{1}),char(objects{4}),char(objects{2}), char(objects{5}));              
             end
         elseif((minDistance==0) && strcmp(xyaxis, 'x-aligned'))
             if (maxy1 - maxy2 > 0)
-                % o2 is above of o1
-                fprintf(outputfid,'Above(%s, %s)\n',objects{5}, objects{4});
+                fprintf(outputfid,'Above,%s,%s,%s,%s \r\n',char(objects{1}),char(objects{4}),char(objects{2}), char(objects{5}));                
             elseif (maxy2 - maxy1 < 0)
-                % o2 is below of o1
-                fprintf(outputfid,'Below(%s, %s)\n',objects{4}, objects{5});
+                fprintf(outputfid,'Below,%s,%s,%s,%s \r\n',char(objects{2}),char(objects{5}),char(objects{1}), char(objects{4}));
             end
         end
 
-	    tline = fgets(fid);
+	    tline = fgetl(fid);
 	end
 	fclose(fid);
     fclose(outputfid);
